3bad840107ecb0a2a547538b8862c848a2c22d50
nimanthahennayake <m2hennayake@gmail.com>
2025-09-27 19:50:31 +0530

    missing href fix

--- frontend/src/features/auth/ui/SignInForm.jsx
+++ frontend/src/features/auth/ui/SignInForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { login } from "@/entities/auth/model/authAPI";
 import fbLogo from "@/shared/assets/images/social_media/facebook-logo.png";
 import googleLogo from "@/shared/assets/images/social_media/google-logo.png";
@@ -68,7 +68,6 @@ export default function SignInForm() {
         />
 
         <div>
-
           <Input
             label="Password"
             id="password"
@@ -83,9 +82,12 @@ export default function SignInForm() {
             inputClass="mt-2 mb-5 p-2.5"
           />
 
-          <a href="/forgot-password" className="text-violet-600 hover:text-violet-700 text-sm transition-colors inline-block mt-0 mb-5">
+          <Link
+            to="/forgot-password"
+            className="text-violet-600 hover:text-violet-700 text-sm transition-colors inline-block mt-0 mb-5"
+          >
             Forgotten password?
-          </a>
+          </Link>
         </div>
 
         <Button
@@ -99,28 +101,31 @@ export default function SignInForm() {
 
         <div className="text-center text-sm mt-5">
           <span>By signing in you agree to our </span>
-          <a
+          <Link
             className="text-navy underline underline-offset-2 decoration-1 decoration-navy-300 hover:text-violet-600 transition-all"
-            href="/legal/terms-of-service/"
+            to="/legal/terms-of-service/"
             target="_blank"
             rel="noreferrer"
           >
             terms of service
-          </a>
+          </Link>
           <span> and </span>
-          <a
+          <Link
             className="text-navy underline underline-offset-2 decoration-1 decoration-navy-300 hover:text-violet-600 transition-all"
-            href="/legal/privacy-policy/"
+            to="/legal/privacy-policy/"
             target="_blank"
             rel="noreferrer"
           >
             privacy policy
-          </a>
+          </Link>
         </div>
 
-        <a href="/signup" className="btn btn-link w-full block text-center text-sm text-violet-600 mt-2">
+        <Link
+          to="/signup"
+          className="btn btn-link w-full block text-center text-sm text-violet-600 mt-2"
+        >
           Need an Account?
-        </a>
+        </Link>
       </form>
     </>
   );
